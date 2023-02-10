b003f9a642fcddf0779a8b95fa2bfcfb5d65208d
Evgeny <solo1evg@mail.ru>
2023-02-10 18:36:21 +0300

    limpié el directorio de prueba

--- src/main/java/ru/work/TestReg.java
+++ src/main/java/ru/work/TestReg.java
@@ -4,23 +4,6 @@ import java.util.*;
 
 public class TestReg {
     public static void main(String[] args) {
-        Person person = new Person("MAY", 11);
-        Person person1 = new Person("MAY",20);
 
-        Map<Person, Integer> personMap = new HashMap<>();
-        personMap.put(person, personMap.getOrDefault(person, 0) + 5000);
-
-        System.out.println(personMap.get("MAY"));
-
-    }
-}
-
-class Person {
-    String name = "";
-    int age = 0;
-
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
     }
 }
